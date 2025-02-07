a9dde6b7e417b230d8d5e1ab8f9230c80f6a933a
Varun Narravula <varun@snare.dev>
2025-02-07 12:12:00 -0800

    feat: first commit, setup env

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Varun's CSC-510 HW at SFSU";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          name = "csc510-shell";
+          packages = with pkgs; [
+            typst
+            typst-fmt
+          ];
+        };
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+      };
+    };
+}
